c90ac610d3a15167ae79c77bba890579cc0bb33f
YunHair <71691550+YunHair@users.noreply.github.com>
2025-04-22 09:51:40 +0800

    Update frp_script.sh

--- trunk/user/frp/frp_script.sh
+++ trunk/user/frp/frp_script.sh
@@ -1,67 +1,43 @@
 #!/bin/sh
-#from hiboy
 export PATH='/etc/storage/bin:/tmp/frp:/tmp/script:/etc/storage/script:/opt/usr/sbin:/opt/usr/bin:/opt/sbin:/opt/bin:/usr/local/sbin:/usr/sbin:/usr/bin:/sbin:/bin'
 export LD_LIBRARY_PATH=/lib:/opt/lib
 killall frpc frps
 mkdir -p /tmp/frp
-#启动frp功能后会运行以下脚本
-#frp项目地址教程: https://github.com/fatedier/frp/blob/master/README_zh.md
-#请自行修改 token 用于对客户端连接进行身份验证
-# IP查询： http://119.29.29.29/d?dn=github.com
 
-
-cat > "/tmp/frp/myfrpc.toml" <<-\EOF
-# ==========客户端配置：==========
+# 生成INI格式配置文件
+cat > "/tmp/frp/myfrpc.ini" <<-\EOF
 [common]
-serverAddr = "frps.com" # 远端frp服务器ip或域名
+server_addr = frps.com
 server_port = 7000
-auth.token = "12345"
+token = 12345
 
-loginFailExit = false
-#log_file = "/tmp/frps.log"
-#log_level = info
-#log_max_days = 2
-
-[[proxies]]
-name = "web"
-type = "http"
-localIP = "192.168.2.1"
-localPort = 80
-subdomain = "test"
-#hostHeaderRewrite = "test.frps.com" #实际你内网访问的域名，可以供公网的域名不一致，如果一致可以不写
-# ====================
+[web]
+type = http
+local_ip = 192.168.2.1
+local_port = 80
+subdomain = test
 EOF
 
-#请手动配置【外部网络 (WAN) - 端口转发 (UPnP)】开启 WAN 外网端口
-cat > "/tmp/frp/myfrps.toml" <<-\EOF
-# ==========服务端配置：==========
-bindAddr = "0.0.0.0"
-bindPort = 7000
-auth.token = "12345"
-# webServer.addr = "127.0.0.1"
-# webServer.port = 7500
-# Dashboard 控制面板用户名密码，默认都为 admin
-# webServer.user = "admin"
-# webServer.password = "admin"
-vhostHTTPPort = 88
-subDomainHost = "frps.com"
-transport.maxPoolCount = 50
-#log.to = "/tmp/frps.log"
-#log.level = "info"
-#log.maxDays = 2
-# ====================
+cat > "/tmp/frp/myfrps.ini" <<-\EOF
+[common]
+bind_port = 7000
+token = 12345
+dashboard_port = 7500
+dashboard_user = admin
+dashboard_pwd = admin
+vhost_http_port = 88
+subdomain_host = frps.com
+max_pool_count = 50
 EOF
 
-#启动：
 frpc_enable=`nvram get frpc_enable`
-frpc_enable=${frpc_enable:-"0"}
 frps_enable=`nvram get frps_enable`
-frps_enable=${frps_enable:-"0"}
+
 if [ "$frps_enable" = "1" ] ; then
-    frps -c /tmp/frp/myfrps.toml >/tmp/frps.log 2>&1 &
+    /etc/storage/bin/frps -c /tmp/frp/myfrps.ini >/tmp/frps.log 2>&1 &
 fi
+
 if [ "$frpc_enable" = "1" ] ; then
     [ "$frps_enable" = "1" ] && sleep 30
-    frpc -c /tmp/frp/myfrpc.toml >/tmp/frpc.log 2>&1 &
+    /etc/storage/bin/frpc -c /tmp/frp/myfrpc.ini >/tmp/frpc.log 2>&1 &
 fi
- 
